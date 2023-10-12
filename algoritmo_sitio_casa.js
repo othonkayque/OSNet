a71b1573f2cba68482efc28584d5e4253f2ef7c4
othonkayque <othoncode@gmail.com>
2023-10-12 16:40:59 -0300

    sitio checkbox add

--- algoritmo_sitio_casa.js
+++ algoritmo_sitio_casa.js
@@ -1,6 +1,9 @@
 var cidade = document.querySelector("#planos-cidade");
+var sitio = document.querySelector("#planos-sitio");
+
 var opt_cidades = document.querySelector("#opt-escolhas")
 var options_cidade_array = ["100 Megas - R$ 69,00","200 Megas - R$ 79,00","300 Megas - R$ 105,00"]
+var options_sitio_array = ["70 Megas - R$ 70,00"]
 
 var verificador_cidade = setInterval(() => {
     if(cidade.checked == true){
@@ -18,4 +21,21 @@ var verificador_cidade = setInterval(() => {
 
         clearInterval(verificador_cidade);
     }    
+    else if(sitio.checked == true){
+        option_select.remove()
+        var option_select = document.createElement("select")
+        option_select.setAttribute("id", "opt-planos")
+        option_select.setAttribute("class", "sitio")
+        opt_cidades.appendChild(option_select)
+        
+        options_sitio_array.forEach(element => {
+            console.log(element)
+            var option_sitio = document.createElement("option");
+            option_sitio.setAttribute("value", 100);
+            option_sitio.innerText = element;
+            option_select.appendChild(option_sitio);
+        });
+
+        clearInterval(verificador_cidade);
+    } 
 }, 1000);
